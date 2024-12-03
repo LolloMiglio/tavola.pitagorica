d673e5eec920329abf3d62417b75bcbfc3318c38
LolloMiglio <lorenzo.migliore@iismajoranamoncalieri.edu.it>
2024-12-03 20:58:45 +0000

    aggiunte modifiche

--- tavolapitagorica.py
+++ tavolapitagorica.py
@@ -2,7 +2,8 @@ numero1 = 1
 while numero1 <= 10:
     numero2 = 1
     while numero2 <= 10:
-        print(numero1 * numero2, end = " ")
+        numero3 = numero1 * numero2
+        print(numero3, end = " ")
         numero2 += 1 
     print()
     numero1 += 1
