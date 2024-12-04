b74e9495b2f798662eba2edae4c84cdbfd14f0cf
LolloMiglio <lorenzo.migliore@iismajoranamoncalieri.edu.it>
2024-12-04 12:27:47 +0100

    Update tavolapitagorica.py
    aggiunto "tab" al posto di "spazio"

--- tavolapitagorica.py
+++ tavolapitagorica.py
@@ -3,7 +3,7 @@ while numero1 <= 10:
     numero2 = 1
     while numero2 <= 10:
         numero3 = numero1 * numero2
-        print(numero3, end = " ")
+        print(numero3, end = "    ")
         numero2 += 1 
     print()
     numero1 += 1
